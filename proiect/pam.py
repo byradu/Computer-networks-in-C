15869d34fd489cc2048b1364bee248c79a3c430d
Zaharioaei Radu <44706212+byradu@users.noreply.github.com>
2021-01-13 02:10:13 +0200

    Delete proiect directory

--- proiect/pam.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-sites = ['https://localhost:2024/','https://localhost:2024/CSSO.txt','https://localhost:2024/links.txt','https://localhost:2024/text.html']
-for i in sites:
-    webbrowser.open(i)
